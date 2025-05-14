72f436b122bc81a434279fe021b75e3a96268c90
David Hoang <davidhoang455@gmail.com>
2025-05-14 18:26:07 +0000

    express install

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 //include Node.js
-const http = require('http');
+const express = require('express');
 
 //set port
 const port = 3000;
 
-//Create server, deal with requests/responses
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
+const app = express();
 
-  switch(req.url){
-    case '/':
-      res.statusCode = 200;
-      res.end('<h1>Welcome to our Home Page</h1><p>Please add interesting info here.</p>');
-    break;
+app.get('/',(req,res)=>{
+	res.send(`
+		<h1>Here's my home page</h1>
+		<p>Welcome to Express!</P>
 
-    case '/about':
-      res.statusCode = 200;
-      res.end('<h1>Welcome to David Hoang\'s About Page</h1><p>Please add interesting info here.</p>');
-    break;
+  `);
 
-    default:
-      res.statusCode = 404;
-      res.end('<h1>File Not Found</h1><p>Please add interesting info here.</p>');
-    break;
-  }
-
-  //default HTML
-  
 });
 
+app.get('/about',(req,res)=>{
+	res.send(`
+		<h1>Here's David's Express About Page</h1>
+		<p>Welcome to Express!</P>
+
+  `);
+
+});
+
+
 //Set server to listen for requests
-server.listen(port, () => {
-  console.log(`Server running at port: ${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
 });
